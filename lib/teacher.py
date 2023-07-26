880451e09127f9a546556329d395c3950a3b385d
Vaschell Mkenzie <mvaschell@gmail.com>
2023-07-26 11:41:56 -0400

    almost finished

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,19 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name,  knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+    ]):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+
     def teach(self):
-        pass
+       self.knowledge
+       return random.randint(0,)
--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
 
 class User:
-    pass
+    # should the knowledge list be put in the user class since both teacher and student need to use it?
+
+    def __init__(self,first_name,last_name):
+        self.first_name = first_name
+        self.last_name = last_name
